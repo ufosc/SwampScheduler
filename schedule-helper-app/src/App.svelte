cc467d8bd7d662483a494458a84fd75678f1c40d
Brian Nielsen <bnielsen1@ufl.edu>
2022-10-27 23:34:34 -0400

    Added working SOC and displayed some of its information

--- schedule-helper-app/src/App.svelte
+++ schedule-helper-app/src/App.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
     import CourseList from './components/CourseList.svelte';
+    import { onMount } from "svelte";
+    import { dataset_dev } from "svelte/internal";
+    import CourseCard from './components/CourseCard.svelte';
+
+    const getSOC = async () => {
+            var response = await fetch('https://one.ufl.edu/apix/soc/schedule/?category=RES&term=2225');
+            var result = await response.json();
+            return result;
+        }
+
+        let SOCPromise = getSOC();
+
+    /*
+        {#each data[0].COURSES as course}
+        <li>{course.name}</li>
+        {/each}
+    */
 </script>
 
 <main class="w-screen h-screen">
@@ -11,4 +28,20 @@
         <!-- schedule -->
         <div class="flex-grow bg-slate-200 rounded-lg"></div>
     </div>
+
+    <h1>Courses</h1>
+      {#await SOCPromise then result}
+      {#each result[0].COURSES as course}
+        <dt>---------------------</dt>
+        <dl>{course.courseId}</dl>
+        <dt>Name: {course.name}</dt>
+        <dt>Description: {course.description}</dt>
+        <dt>Sections: {#each course.sections as section}{section.classNumber} {/each}</dt>
+        <dt></dt>
+        <dt></dt>
+        <dt></dt>
+        <dt></dt>
+      {/each}
+      {/await}
+      <dt>
 </main>
