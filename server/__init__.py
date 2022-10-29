aef6d519a0676bb5fbba51274ebb130a0569464e
Roshan Mehta <sonicroshan122@gmail.com>
2022-10-29 14:09:08 -0400

    feat(server): add proxy to UF SOC Api

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,6 @@
 """Main file for the server."""
+import requests
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
@@ -15,6 +17,21 @@ def get_teacher_ratings():
     teacher_name = request.args.get('name')
     teacher_id = rpm.search_teachers(teacher_name)[teacher_name]
     return jsonify(rpm.get_teacher_ratings(teacher_id))
+
+@APP.route('/schedule/', methods=['GET'])
+@cross_origin()
+def get_schedule():
+    """
+    Returns schedule data by proxing UF SOC API
+    using same query parameters as accesed with this URL.
+    ."""
+    payload = request.args
+    response = requests.get(
+        "https://one.ufl.edu/apix/soc/schedule/",
+        params=payload,
+        timeout=10,
+    )
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     APP.run(debug=True)
