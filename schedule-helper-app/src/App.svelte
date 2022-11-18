9aa37c97cdf9f702477e09f1f5afd4a55d79c08c
Robert Conde <rconde2003@gmail.com>
2022-11-18 16:45:34 -0500

    Changes

--- schedule-helper-app/src/App.svelte
+++ schedule-helper-app/src/App.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-    import CourseList from './components/CourseList.svelte';
+    import DisplaySections from "./components/DisplaySections.svelte";
 </script>
 
 <main class="w-screen h-screen">
     <p class="m-4 text-2xl text-slate-700">Schedule Helper</p>
 
-    <div class="flex flex-row space-x-8 px-4">
-        <CourseList/>
-
-        <!-- schedule -->
-        <div class="flex-grow bg-slate-200 rounded-lg"></div>
-    </div>
+    <DisplaySections/>
 </main>
---
+++ schedule-helper-app/src/components/DisplaySections.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import {Section} from "../scripts/SOC";
+
+    const getSOCJSON = async () => {
+        let resp = await fetch("https://samkoski.000webhostapp.com/uf_fall_2022_soc.json");
+        return resp.json();
+    }
+
+    const getSections = async () => {
+        let socJSON = await getSOCJSON(),
+            sections: Section[] = [];
+
+        socJSON.forEach(function (courseJSON) {
+            courseJSON['sections'].forEach(function (sectionJSON) {
+                sections.push(new Section(sectionJSON));
+            });
+        });
+        return sections;
+    }
+
+    let sections = getSections();
+
+    import {slide} from 'svelte/transition';
+</script>
+
+{#await sections then sections}
+    {#each sections.slice(0, 100) as section}
+        <div class="p-1">
+            <div transition:slide class="inline-block">
+                <!-- Course Card -->
+                <div class="w-full min-h-10 p-2 rounded-lg shadow-sm shadow-slate-400">
+                    <!-- card header -->
+                    <div class="flex flex-row justify-between items-center">
+                        <p class="text-lg text-slate-600">{section.number}</p>
+                    </div>
+
+                    <p class="text-lg text-slate-400">{section.displayName}</p>
+                </div>
+            </div>
+        </div>
+    {/each}
+{/await}
