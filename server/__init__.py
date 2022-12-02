d4e45b109720e59a5dc112e040f6fd9ff4f806d2
Robert Conde <rconde2003@gmail.com>
2022-12-02 16:58:52 -0500

    Reformat

--- server/__init__.py
+++ server/__init__.py
@@ -1,34 +1,36 @@
-"""Main file for the server."""
 import requests
-
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 
 import rate_my_professor
 
-rpm = rate_my_professor.RateMyProfessor()
+rmp = rate_my_professor.RateMyProfessor()
 app = Flask(__name__)
+
 
 @app.route('/teacher-ratings/', methods=['GET'])
 @cross_origin()
 def get_teacher_ratings():
-    """Returns a teacher's rating data based on their name."""
+    """
+    Returns a teacher's rating data based on their name.
+    """
     teacher_name = request.args.get('name')
-    search_result = rpm.search_teachers(teacher_name)
-    
+    search_result = rmp.search_teachers(teacher_name)
+
     if teacher_name in search_result:
         teacher_id = search_result[teacher_name]
-        return jsonify(rpm.get_teacher_ratings(teacher_id))
+        return jsonify(rmp.get_teacher_ratings(teacher_id))
     else:
         return jsonify({})
+
 
 @app.route('/schedule/', methods=['GET'])
 @cross_origin()
 def get_schedule():
     """
-    Returns schedule data by proxing UF SOC API
-    using same query parameters as accesed with this URL.
-    ."""
+    Returns schedule data by proxying UF SOC API
+    using same query parameters as accessed with this URL.
+    """
     payload = request.args
     response = requests.get(
         "https://one.ufl.edu/apix/soc/schedule/",
@@ -36,6 +38,7 @@ def get_schedule():
         timeout=10,
     )
     return jsonify(response.json())
+
 
 if __name__ == '__main__':
     app.run()
