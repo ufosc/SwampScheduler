ea4e980faf3b91cb0a6375f2c664fd803d6eb57d
medricet <medricet@gmail.com>
2022-12-02 16:49:14 -0500

    removed redundant cors object, teacher ratings api now returns empty json instead of crashing on search failure

--- server/__init__.py
+++ server/__init__.py
@@ -2,23 +2,27 @@
 import requests
 
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 
 import rate_my_professor
 
 rpm = rate_my_professor.RateMyProfessor()
-APP = Flask(__name__)
-CORS(APP)
+app = Flask(__name__)
 
-@APP.route('/teacher-ratings/', methods=['GET'])
+@app.route('/teacher-ratings/', methods=['GET'])
 @cross_origin()
 def get_teacher_ratings():
     """Returns a teacher's rating data based on their name."""
     teacher_name = request.args.get('name')
-    teacher_id = rpm.search_teachers(teacher_name)[teacher_name]
-    return jsonify(rpm.get_teacher_ratings(teacher_id))
+    search_result = rpm.search_teachers(teacher_name)
+    
+    if teacher_name in search_result:
+        teacher_id = search_result[teacher_name]
+        return jsonify(rpm.get_teacher_ratings(teacher_id))
+    else:
+        return jsonify({})
 
-@APP.route('/schedule/', methods=['GET'])
+@app.route('/schedule/', methods=['GET'])
 @cross_origin()
 def get_schedule():
     """
@@ -34,4 +38,4 @@ def get_schedule():
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    APP.run(debug=True)
+    app.run(debug=True)
