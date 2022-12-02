142ee09a5f124b12aa3e31e4f06c59334f39fede
medricet <medricet@gmail.com>
2022-12-02 17:32:16 -0500

    formatting cleanup in frontend DisplaySections.svelte, removed unused stores.ts

--- schedule-helper-app/src/scripts/stores.ts
+++
@@ -1,9 +0,0 @@
-import { readable } from 'svelte/store';
-
-export const complete_course_list = readable({
-    // example course
-    '19039': {
-        code: 'AEC3065',
-        name: 'Issues in Agricultural and Life Sciences',
-    }
-}, () => {});
