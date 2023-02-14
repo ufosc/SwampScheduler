3244672059774e77d9bca5e49f32d7b57ebaed10
Robert Conde <rconde2003@gmail.com>
2023-02-14 17:57:22 -0500

    Removed Junk & Back to Tailwind

--- app/src/components/MainDisplay.tsx
+++ app/src/components/MainDisplay.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
 import {Course, SOC} from "../scripts/soc";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CircularProgress from '@mui/material/CircularProgress';
 
 type myStates = {
@@ -51,66 +41,51 @@ export default class MainDisplay extends Component<{}, myStates> {
         const courses = this.state.current_courses.map((course) => {
             const sections = course.sections.map((section) => {
                 return (
-                    <Accordion>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon/>}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <Typography>{section.displayName}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                {section.courseCode}
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
+                    <div className="inline-block m-1">
+                        <div className="w-full min-h-10 p-2 rounded-lg shadow-sm shadow-slate-400">
+                            <div className="flex flex-row justify-between items-center">
+                                <p className="text-lg text-slate-600">{section.number}</p>
+                            </div>
+
+                            <p className="text-lg text-slate-400">{section.displayName}</p>
+                            <p className="text-lg text-slate-400"><i>{section.instructors.join(', ')}</i></p>
+                        </div>
+                    </div>
                 );
             });
 
             return (
-                <>
-                    <div>
-                        <Card sx={{minWidth: 275}}>
-                            <CardContent>
-                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                                    {course.id}
-                                </Typography>
-                                <Typography variant="h5" component="div">
-                                    {course.name}
-                                </Typography>
-                                <Typography sx={{mb: 1.5}} color="text.secondary">
-                                    {course.code}
-                                </Typography>
-                                <Typography variant="body2">
-                                    <>
-                                        {course.description}
-                                        {sections}
-                                    </>
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">Learn More</Button>
-                            </CardActions>
-                        </Card>
+                <div className="mb-3"> {/* COURSES */}
+                    {/* Course Information */}
+                    <p className="mx-4 text-xl text-slate-700 underline"><b>{course.code}</b> {course.name}</p>
+                    <div className="mx-5">
+                        <p className="text-slate-700">{course.description}</p>
                     </div>
-                    <br/>
-                </>
+
+
+                    <div className="mx-6 my-2"> {/* SECTIONS */}
+                        {sections} {/* Section Cards */}
+                    </div>
+                </div>
             )
-        })
+        });
 
         if (courses.length === 0) {
             return (
-                <Box sx={{display: 'flex'}}>
+                <>
                     <b>Loading Courses...</b>
                     <br/>
                     <CircularProgress/>
-                </Box>
-            )
+                </>
+            );
         } else {
             return (
-                <div>{courses}</div>
-            )
+                <>
+                    <p className="m-4 text-2xl text-slate-700">Schedule Helper 📆</p>
+
+                    {courses}
+                </>
+            );
         }
     }
 }
