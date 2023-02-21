47395edcc6c5756144b700ddde5c123eea99931c
Robert Conde <rconde2003@gmail.com>
2023-02-21 17:18:28 -0500

    Added placeholder for course code textbox

--- app/src/components/ScheduleBuilder.tsx
+++ app/src/components/ScheduleBuilder.tsx
@@ -62,6 +62,7 @@ export default class ScheduleBuilder extends Component<propType, stateType> {
                     <div>
                         <input type={"text"}
                                className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5"}
+                               placeholder={"Course Code"}
                                onChange={(event) => this.setState({searchText: event.target.value})}></input>
 
                         <button type="submit"
