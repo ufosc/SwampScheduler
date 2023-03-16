72fcefdfa062ada7d386aa9c0c09c49b29e17a05
Robert Conde <rconde2003@gmail.com>
2023-03-16 13:46:09 -0400

    Changed search box visuals

--- app/src/components/SectionPicker.tsx
+++ app/src/components/SectionPicker.tsx
@@ -106,11 +106,11 @@ export function SectionPicker(props: props) {
         <div>
             <div className="grid grid-cols-3">
                 <div>
-                    <input 
-                        type="text" 
-                        id="first_name" 
-                        className="bg-gray-50 w-48 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="Course code" 
+                    <input
+                        type="text"
+                        id="first_name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5"
+                        placeholder="Course Code"
                         required
                         onChange={(e) => getSearchedSections(e.target.value)}
                     />
