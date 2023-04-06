03b764222784e0b9b26c2280e74ef2c92f100987
Robert Conde <rconde2003@gmail.com>
2023-04-06 17:36:05 -0400

    Extended grid-template-rows-# for 11 rows

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                '11': 'repeat(11, minmax(0, 1fr))'
+            }
+        },
+    },
+    plugins: [],
 }
