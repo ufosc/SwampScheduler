b41ccd1ec1f3bc2fcb4126048081ace7808a3fb9
Robert Conde <rconde2003@gmail.com>
2023-04-06 21:37:23 -0400

    Removed Useless Import

--- app/src/components/ScheduleBuilder.tsx
+++ app/src/components/ScheduleBuilder.tsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Section, SOC} from "../scripts/soc";
 import {Generator, Schedule, Selection} from "../scripts/generator";
-import ScheduleDisplay from "./ScheduleDisplay";
 import SectionPicker from './SectionPicker';
 import MultipleSelectionDisplay from "./MultipleSelectionDisplay";
 import MultipleScheduleDisplay from "./MultipleScheduleDisplay";
