69d002fa4a527afc2521d6a332162a7b7532b727
Robert Conde <rconde2003@gmail.com>
2023-04-06 22:27:50 -0400

    Add TODO

--- app/src/scripts/api.tsx
+++ app/src/scripts/api.tsx
@@ -4,6 +4,7 @@ export class CampusMap {
         return (baseURL + locationID);
     }
 
+    // TODO: add alternate inner (if location is false-ish)
     static createLink(locationID: string, inner: JSX.Element, _target: string = '_blank'): JSX.Element {
         if (locationID) // Exists and is non-empty
             return (<a href={CampusMap.getLocationURL(locationID)} target={_target}>{inner}</a>);
