6e3e10f917b60ee946e93023f36c341376464585
Robert Conde <rconde2003@gmail.com>
2023-07-14 15:35:10 -0700

    Implement `Schedule`.fits(`Section`) and use in Generator

--- app/src/scripts/generator.tsx
+++ app/src/scripts/generator.tsx
@@ -4,6 +4,17 @@ export class Selection extends Array<Section> {
 }
 
 export class Schedule extends Array<Section> {
+    constructor(sections: Section[] = []) {
+        super();
+        if (sections.length > 0)
+            this.push(...sections);
+    }
+
+    fits(sectionToAdd: Section): boolean {
+        return this.every(
+            (sec: Section) => !sectionToAdd.conflictsWith(sec)
+        );
+    }
 }
 
 export class Generator {
@@ -19,7 +30,7 @@ export class Generator {
         console.log("Loaded selections", this.selections);
     }
 
-    async generateSchedules(selectionInd: number = 0, currSchedule: Schedule = [], schedules: Schedule[] = []): Promise<Schedule[]> {
+    async generateSchedules(selectionInd: number = 0, currSchedule: Schedule = new Schedule(), schedules: Schedule[] = []): Promise<Schedule[]> {
         // Return if schedule is complete (or given no selections)
         if (selectionInd == this.selections.length) {
             if (this.selections.length > 0) // Add "schedule" if selections were given (prevents returning an empty schedule when no selections are given)
@@ -27,18 +38,11 @@ export class Generator {
             return schedules;
         }
 
-        // Go through all the sections for the selection, see if it fits the schedule so far
+        // Go through all the sections for the selection, and see if each could generate a new schedule
         let selection: Selection = this.selections[selectionInd];
         for (const sectionToAdd of selection) {
-            let fitsSchedule = true;
-            for (const sec of currSchedule)
-                if (sectionToAdd.conflictsWith(sec)) {
-                    fitsSchedule = false;
-                    break;
-                }
-
-            if (fitsSchedule)
-                await this.generateSchedules(selectionInd + 1, [...currSchedule, sectionToAdd], schedules);
+            if (currSchedule.fits(sectionToAdd)) // If it fits the current schedule, add it and keep going
+                await this.generateSchedules(selectionInd + 1, new Schedule([...currSchedule, sectionToAdd]), schedules);
         }
         return schedules;
     }
