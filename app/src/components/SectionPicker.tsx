30c6944e3ac8e6c2e729b0bde9438c1167ce4571
Hieu <hieunguyen.0527@gmail.com>
2023-09-08 23:20:26 -0400

    update fetching logic

--- app/src/components/SectionPicker.tsx
+++ app/src/components/SectionPicker.tsx
@@ -7,46 +7,62 @@ import { getSearchByStringExample } from "@constants/frontend";
 
 interface Props {
     soc: SOC_Generic;
-    searchText: string;
-    setSearchText: (searchText: string) => void;
 }
 
 export default function SectionPicker(props: Props) {
     const [searchByString, setSearchByString] = useState<string>(
         getSearchByString(SearchBys[0]),
     );
+    const [searchText, setSearchText] = useState("");
     const searchBy = getSearchBy(searchByString);
 
     /* https://github.com/apollographql/apollo-client/issues/9583 */
     const [abortRef, setAbortRef] = useState(new AbortController());
-    const { isFetching, data: courses } = useQuery<Course[]>({
+    const {
+        isFetching,
+        data: courses,
+        refetch: fetchCourses,
+    } = useQuery<Course[]>({
         initialData: [],
-        queryKey: [props.searchText], // Re-fetch when searchText is updated
+        // queryKey: [props.searchText], // Re-fetch when searchText is updated
         queryFn: () => {
             setAbortRef(new AbortController());
-            return props.soc instanceof SOC_API
-                ? props.soc.fetchSearchCourses(
-                      searchBy,
-                      props.searchText,
-                      abortRef,
-                  )
-                : props.soc.searchCourses(searchBy, props.searchText);
+
+            if (props.soc instanceof SOC_API) {
+                return props.soc.fetchSearchCourses(
+                    searchBy,
+                    searchText,
+                    abortRef,
+                );
+            } else {
+                return [];
+            }
+
+            // return props.soc instanceof SOC_API
+            //     ? props.soc.fetchSearchCourses(searchBy, searchText, abortRef)
+            //     : props.soc.searchCourses(searchBy, searchText);
         },
-        enabled: !!props.searchText, // Prevents query when searchText is empty
+        enabled: false,
+        // enabled: !!props.searchText, // Prevents query when searchText is empty
         notifyOnChangeProps: ["data", "isFetching"], // Must re-render on isFetching change to update cursor
         refetchOnWindowFocus: false, // Turned off to prevent queries while debugging using console
         refetchOnReconnect: false, // Not needed
     });
-    console.log(isFetching ? "Fetching courses..." : "Not fetching.");
+    // console.log(isFetching ? "Fetching courses..." : "Not fetching.");
+
+    const searchCourse = () => {
+        fetchCourses();
+    };
 
     // TODO: make it clearer when not fetching and there are no search results
-    const displayCourses = (
-        isFetching
-            ? props.soc instanceof SOC_API
-                ? props.soc.searchCourses(searchBy, props.searchText)
-                : []
-            : courses ?? []
-    ).slice(0, 30);
+    // const displayCourses = (
+    //     isFetching
+    //         ? props.soc instanceof SOC_API
+    //             ? props.soc.searchCourses(searchBy, props.searchText)
+    //             : []
+    //         : courses ?? []
+    // ).slice(0, 30);
+
     return (
         <div
             className={"h-full"}
@@ -78,13 +94,22 @@ export default function SectionPicker(props: Props) {
                         "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 mb-0.5"
                     }
                     placeholder={getSearchByStringExample(searchByString)}
-                    value={props.searchText}
-                    onChange={(e) => props.setSearchText(e.target.value)}
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
                     autoComplete={"off"}
                 />
+
+                <button
+                    onClick={searchCourse}
+                    className="bg-sky-500 hover:bg-sky-400 border border-blue-300 text-white text-sm rounded-lg p-2.5 mr-1"
+                >
+                    Search
+                </button>
             </div>
 
-            <MultipleCourseDisplay courses={displayCourses} />
+            {courses && (
+                <MultipleCourseDisplay courses={courses.slice(0, 30)} />
+            )}
         </div>
     );
 }
