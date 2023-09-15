c93077c2eaf8f295cf9daf08655ec5d290821405
Hieu <hieunguyen.0527@gmail.com>
2023-09-15 02:52:29 -0400

    refactor to using functional component

--- app/src/components/SectionDisplay.tsx
+++ app/src/components/SectionDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Section } from "@scripts/soc";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -10,92 +10,79 @@ import { notNullish } from "@scripts/utils.ts";
 
 interface Props {
     section: Section;
-    draggable: boolean;
-    handleRemove: (sectionToRemove: Section) => void;
+    draggable?: boolean;
+    handleRemove?: (sectionToRemove: Section) => void;
 }
 
-interface States {}
+export default function SectionDisplay({
+    section,
+    draggable = false,
+    handleRemove,
+}: Props) {
+    const allTimes: React.JSX.Element[] = API_Days.map((day, d) =>
+        section.meetings[day].length > 0 ? (
+            <div className="mx-1" key={d}>
+                <b>{day}:</b>{" "}
+                {section.meetings[day].map((mT, m) => (
+                    <span key={m}>
+                        {CampusMap.createLink(
+                            mT.locationID,
+                            `${mT.location}`,
+                            <>{mT.formatPeriods()}</>,
+                        )}{" "}
+                    </span>
+                ))}
+            </div>
+        ) : null,
+    ).filter(notNullish);
 
-const defaultProps = {
-    draggable: false,
-    handleRemove: null,
-};
-
-export default class SectionDisplay extends Component<Props, States> {
-    static defaultProps = defaultProps;
-
-    render() {
-        const section = this.props.section;
-
-        // TODO: refactor
-        let allTimes: React.JSX.Element[] = API_Days.map((day, d) =>
-            section.meetings[day].length > 0 ? (
-                <div className="mx-1" key={d}>
-                    <b>{day}:</b>{" "}
-                    {section.meetings[day].map((mT, m) => (
-                        <span key={m}>
-                            {CampusMap.createLink(
-                                mT.locationID,
-                                `${mT.location}`,
-                                <>{mT.formatPeriods()}</>,
-                            )}{" "}
-                        </span>
-                    ))}
-                </div>
-            ) : null,
-        ).filter(notNullish);
-
-        if (section.isOnline) allTimes.unshift(<b>Online</b>);
-        else if (allTimes.length == 0)
-            // Not online, but no times have been assigned
-            allTimes = [<>TBD</>];
-
-        return (
-            <Draggable
-                className={"inline-block"}
-                type={"uid"}
-                data={section.uid}
-                enabled={this.props.draggable}
-            >
-                <div className="m-1 text-sm">
-                    {" "}
-                    {/* SECTION */}
-                    <div className="w-full p-2 rounded-lg shadow-sm shadow-slate-400">
-                        <div className={"text-slate-600 flex justify-between"}>
-                            <div className={"flex items-center gap-1"}>
-                                <b>{section.number}</b>
-                                <span className={"text-xs align-middle"}>
-                                    ({section.credits.display} Credits)
-                                </span>
-                            </div>
-                            <button
-                                className={"mx-1"}
-                                hidden={!this.props.handleRemove}
-                                onClick={() => this.props.handleRemove(section)}
-                            >
-                                <GrClose />
-                            </button>
+    return (
+        <Draggable
+            className={"inline-block"}
+            type={"uid"}
+            data={section.uid}
+            enabled={draggable}
+        >
+            <div className="m-1 text-sm">
+                {" "}
+                {/* SECTION */}
+                <div className="w-full p-2 rounded-lg shadow-sm shadow-slate-400">
+                    <div className={"text-slate-600 flex justify-between"}>
+                        <div className={"flex items-center gap-1"}>
+                            <b>{section.number}</b>
+                            <span className={"text-xs align-middle"}>
+                                ({section.credits.display} Credits)
+                            </span>
                         </div>
+                        <button
+                            className={"mx-1"}
+                            hidden={!handleRemove}
+                            onClick={() =>
+                                handleRemove && handleRemove(section)
+                            }
+                        >
+                            <GrClose />
+                        </button>
+                    </div>
 
-                        <div className={"text-slate-400"}>
-                            <p className={"flex items-center gap-1"}>
-                                {section.deptControlled && (
-                                    <abbr title={"Departmentally Controlled"}>
-                                        <GrLock />
-                                    </abbr>
-                                )}
-                                {section.displayName}
-                            </p>
-                            <p>
-                                <i>{section.instructors.join(", ")}</i>
-                            </p>
-                            <div className={"flex flex-row justify-around"}>
-                                {allTimes}
-                            </div>
+                    <div className={"text-slate-400"}>
+                        <p className={"flex items-center gap-1"}>
+                            {section.deptControlled && (
+                                <abbr title={"Departmentally Controlled"}>
+                                    <GrLock />
+                                </abbr>
+                            )}
+                            {section.displayName}
+                        </p>
+                        <p>
+                            <i>{section.instructors.join(", ")}</i>
+                        </p>
+                        <div className={"flex flex-row justify-around"}>
+                            {allTimes}
                         </div>
                     </div>
                 </div>
-            </Draggable>
-        );
-    }
+            </div>
+        </Draggable>
+    );
 }
