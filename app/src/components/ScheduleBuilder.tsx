2aa17b57e61b47913ae9fafe842428b6380d4fde
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2024-02-11 20:24:03 -0500

    Update ScheduleBuilder.tsx

--- app/src/components/ScheduleBuilder.tsx
+++ app/src/components/ScheduleBuilder.tsx
@@ -1,9 +1,15 @@
-import { Component } from "react";
-import { Course, Section, SOC_API, SOC_Generic } from "@scripts/soc";
+//Importing React, necessary components, other class files, and any dependencies - used ChatGPT for proper syntax with the 'from ""'. 
+import React, { useState, useEffect } from "react";
 import {
-    Schedule,
-    ScheduleGenerator,
-    Selection,
+  Course,
+  Section,
+  SOC_API,
+  SOC_Generic,
+} from "@scripts/soc";
+import {
+  Schedule,
+  ScheduleGenerator,
+  Selection,
 } from "@scripts/scheduleGenerator";
 import SectionPicker from "@components/SectionPicker";
 import MultipleSelectionDisplay from "@components/MultipleSelectionDisplay";
@@ -13,270 +19,193 @@ import { API_Filters } from "@scripts/apiTypes";
 import { arrayEquals, notEmpty, take } from "@scripts/utils";
 import { LIMIT_VALUES, LIMITS } from "@constants/scheduleGenerator";
 
+//getDefaultSelections: returns an array of schedules selected
+//defaultProgram: default program value (this was already specified in the original code file)
 const getDefaultSelections = () => [new Selection()];
 const defaultProgram = "CWSP";
 
-interface Props {}
+//function to build schedules and accomplish what the ScheduleBuilder class had to achieve before
+const ScheduleBuilder = () => {
+  //filters: API Filters fetched from the UF_SOC_API and are set to null at this time
+  const [filters, setFilters] = useState<API_Filters | null>(null);
+  //soc: Schedule of Courses and is set to null to start off with (since no schedules yet built)
+  const [soc, setSOCState] = useState<SOC_Generic | null>(null);
+  //generator: generation of schedules through user usage of the website
+  const [generator, setGenerator] = useState<ScheduleGenerator | null>(null);
+  //limit: maximum limit of schedules that can be generated; set initially to LIMIT_VALUES[0]
+  const [limit, setLimit] = useState(LIMIT_VALUES[0]);
+  //selections: user-selected course sections
+  const [selections, setSelections] = useState<Selection[]>(
+    getDefaultSelections()
+  );
+  //schedules: initial list of schedules with no schedules at the beginning
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
+  //showAddCourse: if a course needs to be added to a schedule, the AddCourse button would be implemented
+  const [showAddCourse, setShowAddCourse] = useState(false);
+  //pinnedSchedules: if a course wants to be pinned by the user, it will be added to the Schedule array
+  const [pinnedSchedules, setPinnedSchedules] = useState<Schedule[]>([]);
 
-interface States {
-    filters: API_Filters | null;
-    soc: SOC_Generic | null;
-    generator: ScheduleGenerator | null;
-    limit: number;
-    selections: Selection[];
-    schedules: Schedule[];
-    showAddCourse: boolean;
-    pinnedSchedules: Schedule[];
-}
+  useEffect(() => {
+    //fetch filters from UF_SOC_API and set the SOCState with the default program
+    UF_SOC_API.fetchFilters().then(async (fetchedFilters) => {
+      setFilters(fetchedFilters);
+      await setSOCState(fetchedFilters.terms[0].CODE, defaultProgram);
+    });
+  }, []);
+  //allows for loading selected courses into the schedule generator
+  useEffect(() => {
+    if (generator) {
+      generator.loadSelections(
+        selections.filter((sel: Selection) => sel.length > 0)
+      );
 
-const defaultState: States = {
-    filters: null,
-    soc: null,
-    generator: null,
-    limit: LIMIT_VALUES[0],
-    selections: getDefaultSelections(),
-    schedules: [],
-    showAddCourse: false,
-    pinnedSchedules: []
+      const newSchedules: Schedule[] = take(
+        limit,
+        generator.yieldSchedules()
+      );
+      setSchedules(newSchedules);
+      //output whether the schedule was changed or not changed
+      if (schedules !== newSchedules) {
+        console.log("New schedules", newSchedules);
+      } else {
+        console.log("Same schedules");
+      }
+    }
+  }, [limit, selections, generator, schedules]);
+  //reset the schedule building system
+  const reset = () => {
+    console.log("Resetting Schedule Builder");
+    setSelections(getDefaultSelections());
+    setSchedules([]);
+  };
+  //initialize soc variable
+  const setSOC = async (termStr: string, programStr: string) => {
+    console.log(`Setting SOC to "${termStr}" for "${programStr}"`);
+    const socInstance = await SOC_API.initialize({ termStr, programStr });
+    setSOCState(socInstance);
+  };
+  //manages dropping (removing of) a course or schedule 
+  const handleDrop = (ind: number, uid: string) => {
+    if (soc) {
+      const item: Section | Course | null = soc.get(uid);
+      if (item) {
+        let sectionsToTryAdd: Section[];
+        if (item instanceof Course) sectionsToTryAdd = item.sections;
+        else sectionsToTryAdd = [item];
+
+        const sectionsToAdd: Section[] = sectionsToTryAdd.filter(
+          (section) =>
+            !selections.some((sel) => sel.includes(section))
+        );
+        newSelection(ind, sectionsToAdd);
+      }
+    }
+  };
+  //manages the pin status of a schedule
+  const togglePin = (sch: Schedule) => {
+    const pinned = pinnedSchedules;
+    if (pinned.some((s) => arrayEquals(s, sch))) {
+      setPinnedSchedules(pinned.filter((s) => !arrayEquals(s, sch)));
+    } else {
+      setPinnedSchedules([...pinned, sch]);
+    }
+  };
+  //handles adding of new courses or schedules
+  const newSelection = (ind: number = -1, sectionsToAdd: Section[] = []) => {
+    if (ind === -1) {
+      setSelections([...selections, new Selection()]);
+      return;
+    }
+    const newSelections = selections.map((sel, i) => {
+      if (i === ind) return [...sel, ...sectionsToAdd];
+      return sel;
+    });
+    setSelections(newSelections);
+  };
+  //manages deletion of a course selection
+  const handleDeleteSelection = (ind: number) => {
+    let newSelections = selections.filter((_sel, i) => i !== ind);
+    if (newSelections.length === 0) newSelections = getDefaultSelections();
+    setSelections(newSelections);
+  };
+  //manages removal of a course selection
+  const handleRemove = (sectionToRemove: Section) => {
+    const newSelections = selections.map((sel) =>
+      sel.filter((sec) => sec !== sectionToRemove)
+    );
+    setSelections(newSelections);
+  };
+  //sample loading messages - used ChatGPT for assistance with this part
+  return (
+    <div className="min-h-screen flex flex-col h-screen p-3">
+      <div className="flex">
+        <p className="text-2xl text-slate-700 inline-block">
+          🐊 Swamp Scheduler 📆
+        </p>
+
+        <div className="grow"></div>
+
+        <select
+          id="term"
+          className="bg-sky-500 hover:bg-sky-400 border border-blue-300 text-white text-sm rounded-lg p-2.5 mr-1 text-center"
+          defaultValue={soc?.info.termStr}
+          onChange={(e) => setSOC(e.target.value, defaultProgram)}
+          disabled={false}
+        >
+          {filters?.terms.map((t) => {
+            const { term, year } = SOC_Generic.decodeTermString(t.CODE);
+            return (
+              <option value={t.CODE} key={t.CODE}>
+                {term} {year}
+              </option>
+            );
+          })}
+        </select>
+
+        <select
+          id="limit"
+          className="bg-sky-500 hover:bg-sky-400 border border-blue-300 text-white text-sm rounded-lg p-2.5 mr-1 text-center"
+          defaultValue={limit}
+          onChange={(e) => setLimit(Number(e.target.value))}
+          disabled={false}
+        >
+          {LIMITS.map(([num, str]) => (
+            <option value={num} key={num}>
+              Generate ≤{str}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <hr className="my-1.5"></hr>
+
+      <main className="flex flex-row overflow-y-hidden h-full p-1">
+        <div className="overflow-y-auto w-full">
+          <SectionPicker soc={soc} />
+        </div>
+
+        <div className="overflow-y-auto w-full p-1">
+          <MultipleSelectionDisplay
+            selections={selections}
+            handleDrop={handleDrop}
+            newSelection={newSelection}
+            handleRemove={handleRemove}
+            pinnedSchedules={togglePin}
+            handleDeleteSelection={handleDeleteSelection}
+            key={new Date().getTime()}
+          />
+        </div>
+
+        <div className="overflow-y-auto w-full p-1">
+          <MultipleScheduleDisplay
+            schedules={schedules}
+            pin={togglePin}
+            key={new Date().getTime()}
+          />
+        </div>
+      </main>
+    </div>
+  );
 };
-
-export default class ScheduleBuilder extends Component<Props, States> {
-    constructor(props: Props) {
-        super(props);
-        this.state = defaultState;
-    }
-
-    reset() {
-        console.log("Resetting Schedule Builder");
-        this.setState({
-            selections: getDefaultSelections(),
-            schedules: [],
-        });
-    }
-
-    componentDidMount() {
-        // TODO: implement retry upon fetch error
-        UF_SOC_API.fetchFilters().then(async (filters) => {
-            this.setState({ filters });
-            await this.setSOC(filters.terms[0].CODE, defaultProgram);
-        });
-    }
-
-    componentDidUpdate(
-        _prevProps: Readonly<Props>,
-        prevState: Readonly<States>,
-    ) {
-        // If limit was changed or a section was added/removed from a section, generate new schedules
-        if (
-            this.state.limit != prevState.limit ||
-            !arrayEquals(
-                this.state.selections.filter(notEmpty),
-                prevState.selections.filter(notEmpty),
-            )
-        ) {
-            if (this.state.generator) {
-                // Make sure generator is not null
-                this.state.generator.loadSelections(
-                    // Generate schedules from non-empty selections
-                    this.state.selections.filter(
-                        (sel: Selection) => sel.length > 0,
-                    ),
-                );
-
-                const newSchedules: Schedule[] = [
-                    ...take(
-                        this.state.limit,
-                        this.state.generator.yieldSchedules(),
-                    ),
-                ];
-                this.setState({ schedules: newSchedules });
-                console.log(
-                    "Selections were changed, so schedules have been regenerated",
-                    newSchedules,
-                );
-
-                // If schedules changed, log schedules
-                if (prevState.schedules != newSchedules)
-                    console.log("New schedules", newSchedules);
-                else console.log("Same schedules");
-            }
-        }
-    }
-
-    async setSOC(termStr: string, programStr: string) {
-        console.log(`Setting SOC to "${termStr}" for "${programStr}"`);
-        await SOC_API.initialize({ termStr, programStr }).then((soc) =>
-            this.setState({
-                soc: soc,
-                generator: new ScheduleGenerator(soc),
-            }),
-        );
-        this.reset(); // Make sure to only show info from the current SOC
-    }
-
-    async handleDrop(ind: number, uid: string) {
-        if (this.state.soc) {
-            // Make sure SOC exists
-            const item: Section | Course | null = this.state.soc.get(uid);
-            console.log("Handling drop; will try to add", item);
-
-            if (item) {
-                // Make sure a match was found (not null)
-                // Get the section(s) to try to add to selection
-                let sectionsToTryAdd: Section[];
-                if (item instanceof Course) sectionsToTryAdd = item.sections;
-                else sectionsToTryAdd = [item];
-
-                // Get the sections that have not been added to a selection
-                const sectionsToAdd: Section[] = sectionsToTryAdd.filter(
-                    (section) =>
-                        !this.state.selections.some(
-                            // TODO: extract to a Selections class
-                            (sel) => sel.includes(section),
-                        ),
-                );
-                this.newSelection(ind, sectionsToAdd); // Add the section that have not been added
-            }
-        }
-    }
-
-    // TODO: make separate functions
-    togglePin(sch: Schedule) {
-        const pinned = this.state.pinnedSchedules;
-        if (pinned.some(s => arrayEquals(s, sch))) {
-            this.setState({pinnedSchedules: pinned.filter(s => !arrayEquals(s, sch))});
-        } else {
-            this.setState({pinnedSchedules: [...pinned, sch]});
-        }
-    }
-    newSelection(ind: number = -1, sectionsToAdd: Section[] = []) {
-        if (ind == -1) {
-            this.setState({
-                selections: [...this.state.selections, new Selection()],
-            });
-            return;
-        }
-
-        const newSelections = this.state.selections.map((sel, i) => {
-            if (i == ind) return [...sel, ...sectionsToAdd];
-            return sel;
-        });
-        this.setState({ selections: newSelections });
-    }
-
-    handleDeleteSelection(ind: number) {
-        let newSelections = this.state.selections.filter((_sel, i) => i != ind);
-        if (newSelections.length == 0) newSelections = getDefaultSelections();
-
-        this.setState({ selections: newSelections });
-    }
-
-    handleRemove(sectionToRemove: Section) {
-        const newSelections = this.state.selections.map((sel) =>
-            sel.filter((sec) => sec != sectionToRemove),
-        );
-        this.setState({ selections: newSelections });
-    }
-
-    render() {
-        // Show loading screen if filters/terms haven't been fetched yet
-        if (this.state.filters === null)
-            return (
-                <div>
-                    <h1>Fetching latest semester information...</h1>
-                </div>
-            );
-        if (this.state.soc === null)
-            // Make sure SOC is set
-            return (
-                <div>
-                    <h1>Setting latest Schedule of Courses...</h1>
-                </div>
-            );
-
-        return (
-            <div className="min-h-screen flex flex-col h-screen p-3">
-                {/* Title & Term Selector */}
-                <div className="flex">
-                    <p className="text-2xl text-slate-700 inline-block">
-                        🐊 Swamp Scheduler 📆
-                    </p>
-
-                    <div className="grow"></div>
-
-                    <select
-                        id="term"
-                        className="bg-sky-500 hover:bg-sky-400 border border-blue-300 text-white text-sm rounded-lg p-2.5 mr-1 text-center"
-                        defaultValue={this.state.soc.info.termStr}
-                        onChange={(e) =>
-                            this.setSOC(e.target.value, defaultProgram)
-                        }
-                        disabled={false}
-                    >
-                        {this.state.filters?.terms.map((t) => {
-                            const { term, year } = SOC_Generic.decodeTermString(
-                                t.CODE,
-                            );
-                            return (
-                                <option value={t.CODE} key={t.CODE}>
-                                    {term} {year}
-                                </option>
-                            );
-                        })}
-                    </select>
-
-                    <select
-                        id="limit"
-                        className="bg-sky-500 hover:bg-sky-400 border border-blue-300 text-white text-sm rounded-lg p-2.5 mr-1 text-center"
-                        defaultValue={this.state.limit}
-                        onChange={(e) =>
-                            this.setState({ limit: Number(e.target.value) })
-                        }
-                        disabled={false}
-                    >
-                        {LIMITS.map(([num, str]) => (
-                            <option value={num} key={num}>
-                                Generate ≤{str}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-
-                <hr className="my-1.5"></hr>
-
-                {/* Main of Builder */}
-                <main className="flex flex-row overflow-y-hidden h-full p-1">
-                    {/* Picker */}
-                    <div className="overflow-y-auto w-full">
-                        <SectionPicker soc={this.state.soc} />
-                    </div>
-
-                    {/* Selected */}
-                    <div className="overflow-y-auto w-full p-1">
-                        <MultipleSelectionDisplay
-                            selections={this.state.selections}
-                            handleDrop={this.handleDrop.bind(this)}
-                            newSelection={this.newSelection.bind(this)}
-                            handleRemove={this.handleRemove.bind(this)}
-                            pinnedSchedules={this.togglePin.bind(this)}
-                            handleDeleteSelection={this.handleDeleteSelection.bind(
-                                this,
-                            )}
-                            key={new Date().getTime()}
-                        />
-                    </div>
-
-                    {/* Generated Schedules */}
-                    <div className="overflow-y-auto w-full p-1">
-                        <MultipleScheduleDisplay
-                            schedules={this.state.schedules}
-                            pin={this.togglePin.bind(this)}
-                            key={new Date().getTime()}
-                        />
-                    </div>
-                </main>
-            </div>
-        );
-    }
-}
-
+//export the function in other parts - the key purpose behind this task
+export default ScheduleBuilder;
